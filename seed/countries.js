5428c1f44ee2d3c53815dd420ee6391ce380f3d9
Bader Amer <bamer@bloove.com>
2022-04-21 11:11:30 +0300

    comments

--- seed/countries.js
+++ seed/countries.js
@@ -2,5 +2,6 @@ import { countries } from './data/countries.js';
 import Country from '../models/Country.js'
 
 export default async () => {
+    // transaction
     await Country.insertMany(countries)
 }
--- seeder.js
+++ seeder.js
@@ -9,6 +9,13 @@ connect().then(async () => {
     const files = await fs.readdir(path.join(__dirname, '/seed/'))
     for await (const file of files) {
         if (file.endsWith('.js')) {
+            // check the database if the file already seeded before
+            if(!exists) {
+                // save fileName to seeds collection, locked=true
+                // run seeder
+                // 1 minutes
+                // locked=false
+            }
             const seed = await import(path.join(__dirname, `/seed/${file}`))
             await seed.default()
         }
