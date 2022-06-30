c39b1364881f9f6f97a4e8287dffd1f25ef6cce4
maysoonabed <maysoon.abdulqader99@gmail.com>
2022-06-30 17:00:00 +0300

    delete

--- dist/core/redis.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connection = void 0;
-exports.connection = {
-    redis: {
-        host: 'localhost',
-        port: 6379
-    }
-};
